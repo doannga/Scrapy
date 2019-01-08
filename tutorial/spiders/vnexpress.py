e7aa7554c4067f20f76c35c6ab8b419f22ba444a
thethuz <nthethu@gmail.com>
2019-01-08 14:11:56 +0700

    support nga occho in learning data crawl
    Xem mấy cái t comment đi

--- tutorial/spiders/vnexpress.py
+++ tutorial/spiders/vnexpress.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+# cái này lỗi vì trong tutorial, file items không có lớp crawlExpressItem
 from tutorial.items import crawlExpressItem
-
 
 class VnexpressSpider(scrapy.Spider):
     name = 'vnexpress'
@@ -12,7 +12,9 @@ class VnexpressSpider(scrapy.Spider):
     		yield scrapy.Request(url = url, callback = self.parse)
     	pass
     def parse(self, response):
+        #response.xpath("//a[@title]").extract() có phải 1 list k?
     	for title_new in response.xpath("//a[@title]").extract():
+            # định nghĩa lại crawlExpressItem() đi nhé
     	    item = crawlExpressItem()
     	    item['title'] = response.xpath("//a[@title]/text()").extract()
     	    return item
